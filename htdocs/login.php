bb6e46a453c3b6e7e51587cf80e5f36837f8e87b
daikinaito <siriusandjack@yahoo.co.jp[core] autoCRLF = false>
2018-07-13 16:21:37 +0900

    ini変更

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('mbstring.internal_encoding' , 'UTF-8');
+// ini_set('mbstring.internal_encoding' , 'UTF-8');
 header('Content-Type: text/html; charset=UTF-8');
     if(isset($_POST['id']) and isset($_POST['password'])){
 
