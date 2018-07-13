c5bab1d85e491ff7a13ab5cf410beb49e3fe1824
daikinaito <siriusandjack@yahoo.co.jp[core] autoCRLF = false>
2018-07-13 16:31:12 +0900

    データベースお試し

---
+++ htdocs/sample.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8' />
+    <title></title>
+    <style>
+    table {border-collapse: collapse;}
+    th, td {border: solid black 1px;}
+    </style>
+  </head>
+  <body>
+<?php
+$sql = 'SELECT * FROM users WHERE 1'
+
+require_once 'database_conf.php';
+
+$db = new PDO($dsn, $dbUser, $dbPass);
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<table>';
+echo '<tr><th>id</th><th>名前</th><th>パスワード</th>';
+foreach ($result as $person) {
+  $id = $person['id'];
+  $name = $person['name'];
+  $password = $person['password'];
+  echo "<tr><td>$id</td><td>$name</td><td>$password</td></tr>";
+}
+echo '</table>';
+?>
+ </body>
+</html>
